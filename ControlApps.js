2809b6b15aaa43b168ad056e29f05b5b99f1f7a2
muhammadjex07-del <muhammadjex07@gmail.com>
2025-12-29 21:27:08 +0700

    iid
    di

---
+++ ControlApps.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8576430314:AAExsi0NGjdEcOMMrm3GFDvu5wulniddJZY", 
+  owner: "6797348304", 
+  port: "1202", // Ini Wajib Jangan Diubah
+  ipvps: "206.189.144.114" // Jangan Diubah Nanti Eror!!
+};
